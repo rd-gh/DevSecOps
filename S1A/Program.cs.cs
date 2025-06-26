c7fd42b09db7aa85e64ebaa296b4aee186900641
Rohit Dharmale <rohitdharmale@gmail.com>
2025-06-26 11:50:22 +0530

    Add Program.cs

---
+++ S1A/Program.cs.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SecurityDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("SecurityDemo running...");
+        }
+    }
+}
