05fe3ced6516d94baa0bd71dc2599a2f8b57f0be
Rohit Dharmale <rohitdharmale@gmail.com>
2025-06-26 12:23:14 +0530

    Add CodeQl Vul

--- S1A/Program.cs.cs
+++ S1A/Program.cs.cs
@@ -1,12 +1,15 @@
-using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace SecurityDemo
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("SecurityDemo running...");
-        }
-    }
-}
+var builder = WebApplication.CreateBuilder(args);
+
+// Register all required services (including controllers)
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Map controller endpoints
+app.MapControllers();
+
+app.Run();
